4365202093c0cb818f2d23ba851118ea635a49d5
Vishal <vishal23rfx@gmail.com>
2021-10-19 00:17:02 +0530

    added cart-item and displaying number of items in cart icon

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import './cart-dropdown.styles.scss'
+import React from "react";
+import "./cart-dropdown.styles.scss";
 
-import CustomButton from '../custom-button/custom-button.component'
+import CustomButton from "../custom-button/custom-button.component";
+import CartItem from "../cart-item/cart-item.component";
+import { connect } from "react-redux";
 
-function CartDropDown() {
-    return (
-        <div className='cart-dropdown'>
-            <div className='cart-items'/>
-            <CustomButton>Go to Checkout</CustomButton>
-        </div>
-    )
+function CartDropDown({ cartItems }) {
+  return (
+    <div className="cart-dropdown">
+      <div className="cart-items">
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} {...cartItem} />
+        ))}
+      </div>
+      <CustomButton>Go to Checkout</CustomButton>
+    </div>
+  );
 }
 
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
 
-export default CartDropDown
+
+export default connect(mapStateToProps)(CartDropDown);
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
-import './cart-icon.styles.scss'
-import { toggleCartHidden } from '../../redux/cart/cart.action'
-import { connect } from 'react-redux'
+import React from "react";
+import "./cart-icon.styles.scss";
+import { toggleCartHidden } from "../../redux/cart/cart.action";
+import { connect } from "react-redux";
 
-import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
+import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
-function CartIcon({toggleCartHidden}) {
-    return (
-        <div className='cart-icon' onClick={toggleCartHidden} >
-            <ShoppingIcon className='shopping-icon'/>
-            <span className='item-count'>0</span>
-        </div>
-    )
+function CartIcon({ toggleCartHidden,itemCount}) {
+    console.log("Im rerendering");
+  return (
+    <div className="cart-icon" onClick={toggleCartHidden}>
+      <ShoppingIcon className="shopping-icon" />
+      <span className="item-count">{itemCount}</span>
+    </div>
+  );
 }
 
 const mapDispatchToProps = (dispatch) => ({
-    toggleCartHidden: () => dispatch(toggleCartHidden())
-})
+  toggleCartHidden: () => dispatch(toggleCartHidden()),
+});
 
-export default connect(null,mapDispatchToProps)(CartIcon);
+const mapStateToProps = ({ cart: { cartItems } }) => {
+    console.log("Im mapState");
+  return ({itemCount: cartItems.reduce(
+    (totalQuantity, cartItem) => totalQuantity + cartItem.quantity,
+    0)
+  });
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
